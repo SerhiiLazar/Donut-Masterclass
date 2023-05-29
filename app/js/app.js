5679c90060738ba45918fe00fa209f226b09ef5c
Serhii Lazarenok <serhiilazar90@gmail.com>
2023-05-30 00:26:42 +0300

    background image correction and add pop up menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,25 @@
 // // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+// import '~/app/libs/menu/dist/menu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
-
+	burgerMenu();
+	
 })
+
+function burgerMenu() {
+	const mobileMenu = document.querySelector('.js-menu-container');
+	const openMenuBtn = document.querySelector('.js-open-menu');
+	const closedMenuBtn = document.querySelector('.js-close-menu');
+
+	const toggleMenu = () => {
+		const isMenuOpen = 
+			openMenuBtn.getAttribute('aria-expended') === 'true' || false;
+			openMenuBtn.setAttribute('aria-expended', !isMenuOpen);
+			mobileMenu.classList.toggle('is-open'); 
+	}
+
+	openMenuBtn.addEventListener('click', toggleMenu)
+	closedMenuBtn.addEventListener('click', toggleMenu)
+}
