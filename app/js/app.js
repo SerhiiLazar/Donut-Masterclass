297b6ddbbca8947a29387dafc6096f7858dc730b
Serhii Lazarenok <serhiilazar90@gmail.com>
2024-02-26 22:00:28 +0200

    background

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,32 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/menu/dist/menu.js'
+import Swiper from 'swiper/bundle';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+
+	modules: [Navigation, Pagination],
+	// Optional parameters
+	direction: 'vertical',
+	loop: true,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+	// And if we need scrollbar
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  });
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -23,3 +50,5 @@ function burgerMenu() {
 	openMenuBtn.addEventListener('click', toggleMenu)
 	closedMenuBtn.addEventListener('click', toggleMenu)
 }
+
+
