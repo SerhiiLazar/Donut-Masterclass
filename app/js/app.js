2979bbcaeb81e354bdb6042df412fcf41b218d6e
Serhii Lazarenok <serhiilazar90@gmail.com>
2024-02-27 23:09:17 +0200

    adaptive header block

--- app/js/app.js
+++ app/js/app.js
@@ -1,54 +1,20 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/menu/dist/menu.js'
-import Swiper from 'swiper/bundle';
-import { Navigation, Pagination } from 'swiper/modules';
-
-const swiper = new Swiper('.swiper', {
-
-	modules: [Navigation, Pagination],
-	// Optional parameters
-	direction: 'vertical',
-	loop: true,
-  
-	// If we need pagination
-	pagination: {
-	  el: '.swiper-pagination',
-	},
-  
-	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-	// And if we need scrollbar
-	scrollbar: {
-	  el: '.swiper-scrollbar',
-	},
-  });
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-	burgerMenu();
-	
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // Custom JS
+  burgerMenu();
+});
 
 function burgerMenu() {
-	const mobileMenu = document.querySelector('.js-menu-container');
-	const openMenuBtn = document.querySelector('.js-open-menu');
-	const closedMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closedMenuBtn = document.querySelector(".js-close-menu");
 
-	const toggleMenu = () => {
-		const isMenuOpen = 
-			openMenuBtn.getAttribute('aria-expended') === 'true' || false;
-			openMenuBtn.setAttribute('aria-expended', !isMenuOpen);
-			mobileMenu.classList.toggle('is-open'); 
-	}
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expended") === "true" || false;
+    openMenuBtn.setAttribute("aria-expended", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+  };
 
-	openMenuBtn.addEventListener('click', toggleMenu)
-	closedMenuBtn.addEventListener('click', toggleMenu)
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closedMenuBtn.addEventListener("click", toggleMenu);
 }
-
-
